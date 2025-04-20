928fef3b4acff8cf03ff844658262371e444ecec
vodanh2806 <vodanh2806@gmail.com>
2025-04-20 12:10:23 +0700

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -4,7 +4,7 @@ module.exports = {
     lavalink: {
         identifier: 'Sativa',
         password: 'sativa',
-        host: 'http://helya.pylex.xyz/',
+        host: 'helya.pylex.xyz',
         port: 11141,
         secure: false
     }
