2a87e7914e0db6011cf88bda9c43a65ed1947c63
vodanh2806 <vodanh2806@gmail.com>
2025-04-27 12:32:28 +0700

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -2,10 +2,10 @@
 module.exports = {
     enabled: true, 
     lavalink: {
-        identifier: 'GlaceYT',
-        password: 'glaceyt',
-        host: '193.226.78.187',
-        port: 9372,
+        identifier: 'MuzammilOPYT',
+        password: 'yourpasswordhere',
+        host: 'ind1.zapto.org',
+        port: 25575,
         secure: false
     }
 };
